7f19b8ed6ad7c258741d4f95dab60a2a43ac122f
Mercia Jeno <mercia.jeno@gmail.com>
2026-06-23 18:33:37 +0530

    code added

--- src/coffee/CoffeeAsync.java
+++ src/coffee/CoffeeAsync.java
@@ -1,8 +1,59 @@
 package coffee;
 
+import java.util.concurrent.CompletableFuture;
+
 public class CoffeeAsync {
 
 	public static void main(String[] args) {
 		
+		  System.out.println("🥤 Making Espresso...");
+	        makeCoffee(false);
+
+	        System.out.println("\n🥛 Making Latte...");
+	        makeCoffee(true).join();
+
+	        System.out.println("\n🏁 All drinks ready!");
 	}
+	 static CompletableFuture<String> makeCoffee(boolean withMilk) {
+
+	        return CompletableFuture
+	                .supplyAsync(() -> grindBeans())
+	                .thenApply(beans -> brewCoffee(beans))
+	                .thenApply(coffee -> {
+	                    if (withMilk) {
+	                        return addMilk(coffee);
+	                    }
+	                    return coffee;
+	                })
+	                .thenApply(finalDrink -> {
+	                    System.out.println("✅ Served: " + finalDrink);
+	                    return finalDrink;
+	                });
+	    }
+
+	    static String grindBeans() {
+	        sleep(1000);
+	        System.out.println("🫘 Grinding beans...");
+	        return "Ground Beans";
+	    }
+
+	    static String brewCoffee(String input) {
+	        sleep(2000);
+	        System.out.println("☕ Brewing coffee...");
+	        return input + " + Brewed Coffee";
+	    }
+
+	    static String addMilk(String coffee) {
+	        sleep(1500);
+	        System.out.println("🥛 Adding milk...");
+	        return coffee + " + Milk";
+	    }
+
+	    static void sleep(int ms) {
+	        try {
+	            Thread.sleep(ms);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
